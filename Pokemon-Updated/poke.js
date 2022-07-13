3023fd96183cd3104eb0128b7d7b0b68b1d2a6d1
Bibek Chakraborty <bibekbibek0612@gmail.com>
2022-07-13 23:16:21 +0530

    updated pokemon page added

---
+++ Pokemon-Updated/poke.js
@@ -0,0 +1,12 @@
+let b=document.getElementById("ashu");
+b.addEventListener('click',function(e){
+     if(confirm("Do you wanna see Ash's pokemon ?"))
+          window.open("./ash.html")
+    
+});
+let br=document.getElementById("gary");
+br.addEventListener('click',function(e){
+     if(confirm("Do you wanna see Gary's pokemon ?"))
+          window.open("./gary.html")
+    
+});
